648296e61230ff0817f63e1fd39ef6152d505cb3
LakshitXD <lakshitk6666@gmail.com>
2021-08-16 01:18:37 +0530

    parent 4c495053c0d51ae0acca3571f615c8c9417182e8 author LakshitXD <lakshitk6666@gmail.com> 1629056917 +0530 committer kaydee0502 <kshitijdhama513@gmail.com> 1634980094 +0530
    parent 4c495053c0d51ae0acca3571f615c8c9417182e8
    author LakshitXD <lakshitk6666@gmail.com> 1629056917 +0530
    committer kaydee0502 <kshitijdhama513@gmail.com> 1634980064 +0530
    
    parent 4c495053c0d51ae0acca3571f615c8c9417182e8
    author LakshitXD <lakshitk6666@gmail.com> 1629056917 +0530
    committer kaydee0502 <kshitijdhama513@gmail.com> 1634980027 +0530
    
    parent 4c495053c0d51ae0acca3571f615c8c9417182e8
    author LakshitXD <lakshitk6666@gmail.com> 1629056917 +0530
    committer kaydee0502 <kshitijdhama513@gmail.com> 1634979920 +0530
    
    parent 4c495053c0d51ae0acca3571f615c8c9417182e8
    author LakshitXD <lakshitk6666@gmail.com> 1629056917 +0530
    committer kaydee0502 <kshitijdhama513@gmail.com> 1634979807 +0530
    
    🚧 started

--- devsnest/app/resources/api/v1/algo_submission_resource.rb
+++ devsnest/app/resources/api/v1/algo_submission_resource.rb
@@ -3,7 +3,11 @@
 module Api
   module V1
     class AlgoSubmissionResource < JSONAPI::Resource
+<<<<<<< HEAD
       attributes :user_id, :challenge_id, :source_code, :language_id, :test_cases, :total_test_cases, :passed_test_cases, :is_submitted, :status, :total_runtime, :total_memory
+=======
+      attributes :user_id, :challenge_id, :source_code, :language_id, :test_cases, :total_test_cases, :passed_test_cases, :is_submitted
+>>>>>>> parent 4c495053c0d51ae0acca3571f615c8c9417182e8
     end
   end
 end
